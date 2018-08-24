ba54ac3c26c11bf189f8898e2c8722d3b19e67b8
BuildTools <unconfigured@null.spigotmc.org>
2018-08-24 14:52:32 -0400

    Includes repl.it answer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("hello world!");
+        System.out.println("hello world!");
+        System.out.println("Brian Barnefiher");
+        System.out.println("11th Grade");
+        System.out.println("Hocking Hills State Park");
     }
 }
